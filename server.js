d047481f4ffbb4054f52d8c751fefb88730f4913
in-roma <romain.grandjean@outlook.com>
2020-11-07 16:03:54 +0100

    server setup

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const MONGODB_URI = process.env.MONGODB_URI;
 // Modules
 const home = require('./routes/home');
 const events = require('./routes/events');
-const eventTypes = require('./routes/eventtypes');
+// const eventTypes = require('./routes/eventtypes');
 const venues = require('./routes/venues');
 const artists = require('./routes/artists');
 const users = require('./routes/users');
@@ -29,7 +29,7 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use('/', home);
 app.use('/api/events', events);
-app.use('/api/eventtypes', eventTypes);
+// app.use('/api/eventtypes', eventTypes);
 app.use('/api/venues', venues);
 app.use('/api/artists', artists);
 app.use('/api/users', users);
